76d6f9aebda7ce6a46fa4a4dcc293e351a8de861
Dillan Prince <dillan.prince@buildasign.com>
2019-05-13 15:14:10 -0500

    add heroku postbuild

--- src/client/src/setupProxy.js
+++ src/client/src/setupProxy.js
@@ -1,4 +1,4 @@
-const proxy = require('http-proxy-middleware');
-module.exports = (app) => {
+import proxy from 'http-proxy-middleware';
+export default (app) => {
   app.use(proxy('/api/*', { target: 'http://localhost:5000/' }));
 };
