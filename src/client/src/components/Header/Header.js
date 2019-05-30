7c41f26494592fcf1284ae10391d5ab6b3e8c20b
Jeanie Keonine <jeanie.keonine@buildasign.com>
2019-05-29 23:44:30 -0500

    test push, fix dev console warning

--- src/client/src/components/Header/Header.js
+++ src/client/src/components/Header/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <header class="primary-header">
+      <header className="primary-header">
         <h1>Welcome to the badass quote generator</h1>
       </header>
     );
