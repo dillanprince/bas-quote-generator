b8e68cb82d3a9f67c5dba2152297843574aa96d8
Jeanie Keonine <jeanie.keonine@buildasign.com>
2019-05-31 09:16:44 -0500

    wire up pdf api

--- src/server/index.mjs
+++ src/server/index.mjs
@@ -1,27 +1,25 @@
 import express from 'express';
 import path from 'path';
+import pdfApi from './api/htmlToPdf';
 
 const app = express();
-
-app.get('/api', (req, res) => {
-  console.log('get request to "/" handled.');
-  res.send();
-});
+pdfApi(app, express.Router());
 
 // set up express for production
 if (process.env.NODE_ENV === 'production') {
+  const clientPath = 'src/client/build';
+
   // express will serve production assets like main.js
-  app.use(express.static('src/client/build'));
+  app.use(express.static(clientPath));
 
   // express will serve the index.html file if it doesn't recognize the route
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve('src/client/build/index.html'));
+    res.sendFile(path.resolve(`${clientPath}/index.html`));
   });
 }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, (err) => {
-  return err
-    ? console.log(err)
-    : console.log(`Server listening on port ${PORT}`);
+  const msg = err ? err : `Server listening on port ${PORT}`; 
+  return console.log(msg);
 });
